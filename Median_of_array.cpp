a22d278ce373a9e38f96fe80940387ca23a6e493
absaralam17-code <absaralam2020m1@gmail.com>
2026-06-06 15:12:49 +0530

    Add Median of Two Sorted Arrays solution

---
+++ Median_of_array.cpp
@@ -0,0 +1,82 @@
+/*Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+Example 2:
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+auto findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) -> double {
+    if (nums1.size() > nums2.size()) {
+        return findMedianSortedArrays(nums2, nums1);
+    }
+    
+    int n1 = nums1.size();
+    int n2 = nums2.size();
+    int low = 0, high = n1;
+    
+    while (low <= high) {
+        int cut1 = (low + high) / 2;
+        int cut2 = (n1 + n2 + 1) / 2 - cut1;
+        
+        int l1 = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
+        int l2 = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
+        int r1 = (cut1 == n1) ? INT_MAX : nums1[cut1];
+        int r2 = (cut2 == n2) ? INT_MAX : nums2[cut2];
+        
+        if (l1 <= r2 && l2 <= r1) {
+            if ((n1 + n2) % 2 == 0) {
+                return (max(l1, l2) + min(r1, r2)) / 2.0;
+            } else {
+                return static_cast<double>(max(l1, l2));
+            }
+        } else if (l1 > r2) {
+            high = cut1 - 1;
+        } else {
+            low = cut1 + 1;
+        }
+    }
+    return 0.0;
+}
+
+int main() {
+    int size1, size2;
+
+    cout << "Enter size of first sorted array: ";
+    cin >> size1;
+    vector<int> nums1(size1);
+    cout << "Enter " << size1 << " sorted elements: ";
+    for (int i = 0; i < size1; i++) {
+        cin >> nums1[i];
+    }
+
+    cout << "Enter size of second sorted array: ";
+    cin >> size2;
+    vector<int> nums2(size2);
+    cout << "Enter " << size2 << " sorted elements: ";
+    for (int i = 0; i < size2; i++) {
+        cin >> nums2[i];
+    }
+
+    auto median = findMedianSortedArrays(nums1, nums2);
+
+    cout << "The median is: " << median << endl;
+
+    return 0;
+}
