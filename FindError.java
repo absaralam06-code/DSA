d501535c8a2e00fb6466f1b4387baf3767854333
Absar Alam <absaralam12092006@gmail.com>
2026-06-14 20:47:33 +0530

    leetcode - 625

---
+++ FindError.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class FindError{
+
+    public static int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        int[] freq = new int[n + 1];
+
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        int duplicate = 0;
+        int missing = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (freq[i] == 2) {
+                duplicate = i;
+            }
+            if (freq[i] == 0) {
+                missing = i;
+            }
+        }
+
+        return new int[]{duplicate, missing};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 2, 4};
+
+        int[] result = findErrorNums(nums);
+
+        System.out.println("Duplicate Number: " + result[0]);
+        System.out.println("Missing Number: " + result[1]);
+    }
+}
