94284a195361e3c8ba834597e5a797eabd6ba29d
Absar Alam <absaralam12092006@gmail.com>
2026-07-04 16:59:42 +0530

    lc_169

---
+++ Lc_169.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Lc_169 {
+
+    public static int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.println("Majority Element: " + majorityElement(nums));
+
+        sc.close();
+    }
+}
